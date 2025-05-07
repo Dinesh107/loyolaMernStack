8e7dd47f6af96d2bc2d24500556cc01bac908af0
Dinesh107 <dineshjr107@gmail.com>
2025-05-07 11:24:40 +0530

    Second Commit

---
+++ index.js
@@ -0,0 +1,166 @@
+// Computer Lang - Binary number system 
+
+// Low level language -- 0 1 
+
+// 0 - OFF 
+// 1 - ON 
+
+// 0000 010101010101
+// 010101010101001
+
+// 101001010101001
+
+
+// High level lang ---> Interpretor and Compiler 
+
+// cout()
+// printf 
+// System.out.println()
+// print()
+// console.log()
+
+// console.log("Welcome to js workshop");
+// console.log("I love briyani");
+// console.log("Love to play Cricket");
+// console.log("haii");
+
+// window.alert("Selva is the student");
+
+// document.getElementById("myH1").textContent = "Hello Students";
+
+// document.getElementById("myP").textContent = "Welcome to js class";
+
+// let age = 21;
+// let price = 4.5;
+// let cgpa = 8.0;
+
+// console.log("Your age is  age");
+// console.log("The price is " + price);
+// console.log(`Your cgpa is ${cgpa}, your cpga is good`);
+
+// let age = 10; 
+
+// if(age >= 18) {
+//     console.log("You are eigible for vote");
+// } else {
+//     console.log("You are not eigible for vote");
+// }
+
+// let mark = 99;
+
+// if(mark < 20 ) { 
+//     console.log("You are grade is D, you need imporove");   
+// } else if(mark >= 20 && mark < 50) {
+//     console.log("You are grade is C, you are good"); 
+// } else if(mark >= 50 && mark < 70) {
+//     console.log("You are grade is B, you very good"); 
+// } else if(mark > 70 && mark <= 100) {
+//     console.log("You are grade is A, Awesomee!!!"); 
+// }
+
+// if(age >= 18) {
+//     console.log("You are eigible for vote");
+// } else {
+//     console.log("You are not eigible for vote");
+// }
+
+// let age = 20;
+
+// let message = age >= 18? "You are eigible for vote" : "You are not eigible for vote"; 
+
+// console.log(message);
+
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// let day = 100;
+
+// switch(day) {
+//     case 1:
+//         console.log("It is monday");
+//         break;
+//     case 2:
+//         console.log("It is Tuesday");
+//         break;
+//     case 3:
+//         console.log("It is Wed");
+//         break;
+//     case 4:
+//         console.log("It is Thurs");
+//         break;
+//     case 5:
+//         console.log("It is Friday");
+//         break;
+//     default:
+//         console.log("Invalid input");
+//         break;     
+// }
+
+
+//  index = 0 - n-1
+// 0 - 4
+// n = length = 5
+
+
+// let username = "Selva";
+
+// indexOf()
+
+// console.log(username.indexOf("a"));
+// console.log(username.lastIndexOf("a"));
+// // Trim()
+// console.log(username);
+// console.log(username.trim());
+
+
+
+// console.log(username.toLowerCase());
+// console.log(username.toUpperCase());
+
+let username = "Selva123445666432 Student";
+
+// let firstName = username.slice(0, 5);
+// console.log(`Your first name is ${firstName}`);
+
+// let lastName = username.slice(6);
+// console.log(`Your last name is ${lastName}`);
+// let firstName = username.slice(0, username.indexOf(" "));
+// console.log(`Your first name is ${firstName}`);
+
+// let lastName = username.slice(6);
+// console.log(`Your last name is ${lastName}`);
+
+// email = selva1234@gmail.com
+// Task :- Extarct the username only from that email id and log the output
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
